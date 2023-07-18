db4a23699840a6bd83bf84cf6eaba1d80d485fb7
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-18 16:46:46 +0530

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,14 @@
+/* do-while Loop Statement...! */
+class DoWhile
+{
+    public static void main(String[] args) {
+
+        int n=1;
+        do
+        {
+            System.out.println(n+" ");
+            ++n;
+        }
+        while(n<=10);
+    }
+}
